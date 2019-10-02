f428adea219679aca572e284fe49e67530728bd4
Sergio Ruiz <sergioruiz@pop-os.localdomain>
2019-10-02 11:11:22 -0500

    currency_plugin

---
+++ plugin/src/function.py
@@ -0,0 +1,62 @@
+import requests
+import time
+
+def main(kwargs):
+
+    print("[INFO] Info recieved: {}".format(kwargs))
+    
+    if len(kwargs) < 4:
+        print("[ERROR] One or more parameters are missing")
+        return {"status": "error"}
+    
+    result = get_currency(**kwargs)
+
+    args = result.get("rates")
+
+    print (args)
+       
+    req = update_device(args, **kwargs)
+    del kwargs
+
+    return req.json()
+
+def get_currency(currencies, base, _plugin_env_API_URL, **kwargs):
+    
+    url = "{}/latest?base={}&symbols={}".format(_plugin_env_API_URL, base, currencies)
+    headers = {"Content-Type": "application/json"}
+    req = create_request(url, headers, attempts=5, request_type="get")
+    return req.json()
+
+def update_device(payload, _plugin_env_UBIDOTS_URL, deviceLabel, ubidotsToken, **kwargs):
+    """
+    updates a variable with a single dot
+    """
+    url = "{}/api/v1.6/devices/{}".format(_plugin_env_UBIDOTS_URL, deviceLabel)
+    headers = {"X-Auth-Token": ubidotsToken, "Content-Type": "application/json"}
+    req = create_request(url, headers, attempts=5, request_type="post", data=payload)
+    return req
+
+def create_request(url, headers, attempts, request_type, data=None):
+    """
+    Function to make a request to the server
+    """
+    request_func = getattr(requests, request_type)
+    kwargs = {"url": url, "headers": headers}
+    if request_type == "post" or request_type == "patch":
+        kwargs["json"] = data
+    try:
+        req = request_func(**kwargs)
+        print("[INFO] Request result: {}".format(req.text))
+        status_code = req.status_code
+        time.sleep(1)
+        while status_code >= 400 and attempts < 5:
+            req = request_func(**kwargs)
+            print("[INFO] Request result: {}".format(req.text))
+            status_code = req.status_code
+            attempts += 1
+            time.sleep(1)
+        return req
+    except Exception as e:
+        print("[ERROR] There was an error with the request, details:")
+        print(e)
+        return None
